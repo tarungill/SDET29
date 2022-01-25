8c6da14ff9ff03cb81e58dc360e53e681abd0b5b
lenovo <lenovo@TARUN>
2022-01-24 19:51:56 -0800

    user1 first commit

--- src/test/java/DemoGit/DemoGit.java
+++ src/test/java/DemoGit/DemoGit.java
@@ -1,5 +1,15 @@
 package DemoGit;
 
+import org.testng.annotations.Test;
+
 public class DemoGit {
+	
+	@Test
+	public void demoGit()
+	{
+		
+		System.out.println("Hii this user1 first commit");
+		
+	}
 
 }
